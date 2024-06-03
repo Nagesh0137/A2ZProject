af0577c8d317cdd396801a631b38044fc09b11f6
Nagesh balu Sonawane <nageshsonawane870@gmail.com>
2024-06-03 19:06:22 +0530

    commit

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -4,7 +4,7 @@ function Blog(props) {
   return (
     <>
       <div className="card mt-5" style={{ width: "18rem;" }}>
-        <img src={props.img} className="card-img-top" alt="img card" />
+        <img src={props.img} className="card-img-top" alt="img" />
         <div className="card-body">
           <h5 className="card-title">{props.title}</h5>
           <p className="card-text">{props.desc}</p>
